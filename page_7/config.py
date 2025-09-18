4b399a9e27e0523baf2ccc34b194893b01f401d8
Evgeniy Kungurov <johnk2280@gmail.com>
2025-09-18 16:55:06 +0500

    [add] page_7

--- page_7/config.py
+++ page_7/config.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 
 from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore',
+    )
+
     BASE_DIR: Path = Path(__file__).resolve()
     ENVIRONMENT: str = 'dev'
     LOG_LEVEL: str = 'DEBUG'
